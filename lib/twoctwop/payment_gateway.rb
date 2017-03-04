5e05ce78e88aa14d87e0d6e31e905ca3842f305f
Eugie Limpin <eugiellimpin@gmail.com>
2017-03-05 02:05:57 +0700

    Outline
    Signed-off-by: Eugie Limpin <eugiellimpin@gmail.com>

--- lib/twoctwop/payment_gateway.rb
+++ lib/twoctwop/payment_gateway.rb
@@ -15,13 +15,35 @@ module Twoctwop
     end
 
     class Configuration
+    # - merchant_id
+    # - twoctwop endpoint
+    # - onetwothree endpoint
+    # - passphrase
+    # - private key
+
       attr_accessor :merchant_id
     end
 
     class Request
+    # - accept a payload
+    # - create request steps
+    # - execute request steps
+    # - return Response
+
       def merchant_id
         Twoctwop::PaymentGateway.configuration.merchant_id
       end
     end
+
+    # RequestStep
+    #
+
+    # Response
+    # - decrypts paymentResponse
+    # - transform XML paymentResponse to a Hash with snake case keys
+
+    # Payload
+    # - generate a hash(?)
+    # - generate XML payload
   end
 end
