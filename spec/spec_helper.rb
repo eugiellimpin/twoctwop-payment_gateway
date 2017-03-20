1d20c093774134f0c1beb8ef2eb9f15b268518d8
Eugie Limpin <eugiellimpin@gmail.com>
2017-03-20 22:47:35 +0800

    Require new modules on tests
    Signed-off-by: Eugie Limpin <eugiellimpin@gmail.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,4 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "twoctwop/payment_gateway"
+require "twoctwop/payment_gateway/payload"
+require "twoctwop/payment_gateway/request_step"
